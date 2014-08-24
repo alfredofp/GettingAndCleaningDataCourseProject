f76c634c616ee4724ba52e02d6d645132e843f54
Alfredo Font <alfredo.font@gmail.com>
2014-08-24 19:53:16 +0200

    coursera project uploading

---
+++ codeBook.md
@@ -0,0 +1,31 @@
+##Introduction
+
+The script run_analysis.R
+
+* downloads the data from "UCI Machine Learning Repository"
+* merges the training and test sets to create one data set
+* replaces "activity" values in the dataset with descriptive activity names
+* extracts only the measurements (features) on the mean and standard deviation for each measurement
+* appropriately labels the columns with descriptive names
+* creates a second, independent tidy dataset with an average of each variable for each each activity and each subject. In other words, same type of measurements for a particular subject and activity are averaged into one value and the tidy data set contains these mean values only. The processed tidy data set is also exported as csv file.
+
+##run_analysis.R
+All the steps mentioned above are integrated within the script and performed sequentially. The script also assumes that "plyr" library is already installed.
+
+##The original data set
+The original data set is split into training and test sets (70% and 30%, respectively) where each partition is also split into three files that contain
+
+* measurements from the accelerometer and gyroscope
+* activity label
+* identifier of the subject
+
+##Getting and cleaning data
+If the data is not already available in the data directory, it is downloaded from UCI repository.
+
+The first step is to obtaining data from files, and add descriptive names to column names, also activity labels are replaced with descriptive activity names, defined in "activity_labels.txt" in the original data folder.
+
+The next step of the preprocessing is to merge the training and test sets. Two sets combined, there are approximately 10,000 observations where each instance contains 561 features (560 measurements and subject identifier). After the merge operation the resulting data, the table contains 562 columns (560 measurements, subject identifier and activity label have been added).
+
+After the merge operation, mean and standard deviation features are extracted for further processing. Out of 560 measurement features, mean and standard deviations features are extracted, yielding a data frame with 88 features (additional two features are subject identifier and activity label).
+
+The final step creates a tidy data set with the average of each variable for each activity and each subject. All observations are split into groups (the number of groups  depends on the number of subjects and activities) and mean and standard deviation features are averaged for each group. The resulting data table has 54 rows and 88 columns.
---
+++ run_analysis.R
@@ -0,0 +1,72 @@
+## run_analysis.R
+## Getting and Cleaning Data course assignment.
+##
+## 1.Merges the training and the test sets to create one data set.
+## 2.Extracts only the measurements on the mean and standard deviation for each
+## measurement. 
+## 3.Uses descriptive activity names to name the activities in the data set
+## 4.Appropriately labels the data set with descriptive variable names. 
+## 5.Creates a second, independent tidy data set with the average of each 
+## variable for each activity and each subject. 
+
+## library used to apply mean function to the tidy dataset, ddply function
+library(plyr)
+
+## data is read
+dataTest <- read.table("./UCIHARDataset/test/X_test.txt")
+dataTrain<- read.table("./UCIHARDataset/train/X_train.txt")
+
+##variable names
+variableNames <- readLines("./UCIHARDataset/features.txt")
+
+## variableNames corresponds to the names of columns within datasets, so 
+## for the sake of clarity, the column names are changed
+
+## before changing the name, we obtain a tidy name from the variableNames
+## strings splitted by " ", we retain the second element
+properNames <- sapply(strsplit(variableNames, " "), function(x){x[2]})
+names(dataTest) <- properNames
+names(dataTrain) <- properNames
+
+## test and training labels are read
+testLabels <- as.numeric (readLines("./UCIHARDataset/test/y_test.txt"))
+trainLabels <- as.numeric (readLines("./UCIHARDataset/train/y_train.txt"))
+
+## and so are their names, by removing their numbers
+activityLabels <- readLines("./UCIHARDataset/activity_labels.txt")
+activityLabels <- sapply(strsplit(activityLabels, " "), function(x){x[2]})
+
+## numbers in testLabels and trainLabels corresponds to activities
+## described within activityLabels, we are going to replace the numbers 
+## for the corresponding activity
+replacebyname <- function(x, names){names[x]}
+testActivityLabels <- sapply (testLabels, replacebyname, names=activityLabels)
+trainActivityLabels <- sapply (trainLabels, replacebyname, names=activityLabels)
+## a column is added to each dataset including the activity
+dataTest$activity <-testActivityLabels
+dataTrain$activity <-trainActivityLabels
+
+
+## subject Id labels are read
+subjectTest <- readLines("./UCIHARDataset/test/subject_test.txt")
+subjectTrain <- readLines("./UCIHARDataset/train/subject_train.txt")
+
+## and added as another column.
+dataTest$subject <-subjectTest
+dataTrain$subject<- subjectTrain
+
+
+## only columns related to mean or std are maintained in tidy dataset
+tidyTest <- dataTest[grep(".*[mM]ean.*|.*[sS]td.*|activity|subject", 
+                          names(dataTest))]
+tidyTrain <- dataTest[grep(".*[mM]ean.*|.*[sS]td.*|activity|subject", 
+                          names(dataTrain))]
+
+##and data is joined into one dataset.
+tidyData<- rbind (tidyTrain, tidyTest)
+
+## the mean for each variable, subject and activity is obtained
+result <- ddply(tidyData, .(activity, subject), numcolwise(mean))
+
+## the result is stored into a .txt file
+write.table(result, file="./tidymeanstd.txt",row.name=FALSE, sep = "\t", append=F)
